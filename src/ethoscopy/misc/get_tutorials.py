e95b33cb028164ebff851cf9e33da57941471e03
Blackhurst Laurence <l.blackhurst19@imperial.ac.uk>
2022-11-14 20:05:10 +0000

    added tutorial pickles and function to get

---
+++ src/ethoscopy/misc/get_tutorials.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from pathlib import PurePath
+
+def get_tutorial(type):
+    """ Get a dataset for use with the tutorials avaibale at : http://lab.gilest.ro/ethoscopy"""
+    path = PurePath(__file__)
+    this_dir = path.parent
+
+    data_path = this_dir / f'tutorial_data/{type}_data.pkl'
+    meta_path = this_dir / f'tutorial_data/{type}_meta.pkl'
+
+    return pd.read_pickle(meta_path), pd.read_pickle(data_path)
