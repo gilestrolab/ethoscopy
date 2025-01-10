2f9b68b462bbc7c446c1278c84d149e4e6f1d83c
Lblackhurst29 <l.blackhurst19@imperial.ac.uk>
2025-01-10 13:40:05 +0000

    updated the init file to be more readable and informative

--- src/ethoscopy/__init__.py
+++ src/ethoscopy/__init__.py
@@ -1,8 +1,37 @@
+"""
+Ethoscopy: A Python package for behavioral analysis of Drosophila data.
+
+This package provides tools for loading, analyzing and visualizing ethoscope data.
+"""
+
 from ethoscopy.behavpy import behavpy
-from ethoscopy.load import download_from_remote_dir, link_meta_index, load_ethoscope, load_ethoscope_metadata
-from ethoscopy.analyse import max_velocity_detector, sleep_annotation, stimulus_response, stimulus_prior
+from ethoscopy.load import (
+    download_from_remote_dir,
+    link_meta_index,
+    load_ethoscope,
+    load_ethoscope_metadata
+)
+from ethoscopy.analyse import (
+    max_velocity_detector,
+    sleep_annotation,
+    stimulus_response,
+    stimulus_prior
+)
 from ethoscopy.misc.general_functions import concat
 
 import importlib.metadata
 
 __version__ = importlib.metadata.version("ethoscopy")
+
+__all__ = [
+    'behavpy',
+    'download_from_remote_dir',
+    'link_meta_index',
+    'load_ethoscope',
+    'load_ethoscope_metadata',
+    'max_velocity_detector',
+    'sleep_annotation',
+    'stimulus_response',
+    'stimulus_prior',
+    'concat'
+]
